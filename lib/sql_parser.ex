a94ab8f13d7bf00899e0414e3aef4e8522edd31e
Joshua Wood <josh.nz@gmail.com>
2024-03-02 20:00:17 +1300

    Add choice combinator.

--- lib/sql_parser.ex
+++ lib/sql_parser.ex
@@ -1,19 +1,37 @@
 defmodule SqlParser do
   def run() do
-    input = "select foo from bar"
+    input = "foo_1 bar_2"
     IO.puts("input: #{inspect(input)}\n")
     parse(input)
   end
 
   defp parse(input) do
-    parser = char(?_)
+    parser = choice([ascii_letter(), char(?_), digit()])
     parser.(input)
   end
 
-  defp digit(), do: satisfy(char(), fn char -> char in ?0..?9 end)
-  defp ascii_letter(), do: satisfy(char(), fn char -> char in ?A..?Z or char in ?a..?z end)
-  defp char(expected), do: satisfy(char(), fn char -> char == expected end)
+  # Sometimes called `one-of`.
+  defp choice(parsers) do
+    fn input ->
+      case parsers do
+        [] ->
+          {:error, "No parser succeeded"}
 
+        [first_parser | other_parsers] ->
+          with {:error, _reason} <- first_parser.(input),
+               do: choice(other_parsers).(input)
+      end
+    end
+  end
+
+  defp digit(), do: satisfy(read_char(), fn char -> char in ?0..?9 end)
+  defp ascii_letter(), do: satisfy(read_char(), fn char -> char in ?A..?Z or char in ?a..?z end)
+  defp char(expected), do: satisfy(read_char(), fn char -> char == expected end)
+
+  # If a combinator takes parser/s, it must invoke
+  # them whilst also returning a function. This sets
+  # up the combinator function call chain eg 
+  # foo(bar(baz(quax(read_char))))
   defp satisfy(parser, acceptor) do
     fn input ->
       with {:ok, term, rest} <- parser.(input) do
@@ -27,7 +45,7 @@ defmodule SqlParser do
   # A combinator is a function that returns a parser
   # according to the parser contract, defined below.
   # This function char/0 is a combinator.
-  defp char() do
+  defp read_char() do
     # This anonymous function is a parser.
     fn input ->
       case input do
